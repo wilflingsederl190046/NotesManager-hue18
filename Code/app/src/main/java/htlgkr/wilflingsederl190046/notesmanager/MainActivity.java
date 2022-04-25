c2c19f594791ca0b42d9667281d18661f178617c
wilflingsederl190046 <wilflingsederl190046@sus.htl-grieskirchen.at>
2022-04-25 10:08:09 +0200

    first commit with default project

---
+++ Code/app/src/main/java/htlgkr/wilflingsederl190046/notesmanager/MainActivity.java
@@ -0,0 +1,14 @@
+package htlgkr.wilflingsederl190046.notesmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
